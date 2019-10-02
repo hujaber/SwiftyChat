25c188333cae585d699594b6f929c5e3d8a2a187
Hussein Jaber <hujaber@me.com>
2019-10-02 14:57:27 +0300

    Initial Commit

---
+++ SwiftyChat/SwiftyChat.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyChat.h
+//  SwiftyChat
+//
+//  Created by Hussein Jaber on 2/10/19.
+//  Copyright © 2019 Hussein Jaber. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyChat.
+FOUNDATION_EXPORT double SwiftyChatVersionNumber;
+
+//! Project version string for SwiftyChat.
+FOUNDATION_EXPORT const unsigned char SwiftyChatVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyChat/PublicHeader.h>
+
+
